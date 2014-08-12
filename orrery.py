678230b70f332d2e60bdcb4cb5a40a97cad71f95
Pete Haughie <speedwolf@gmail.com>
2014-08-12 23:40:26 +0100

    Describe the World and its attendant bodies.

--- orrery.py
+++ orrery.py
@@ -1,61 +1,120 @@
+# constants
+SCREEN_SIZE = (640, 480)
+
+# count every man made star
+SAT_COUNT = (0)
+
 import math
 from math import *
 
 import pygame
 from pygame.locals import *
+
+from gameobjects.vector2 import Vector2
+
 from sys import exit
 
-pygame.init()
+class World(object):
+  
+  def __init__(self):
+    self.entities = {}
+    self.entities_id = 0
+    self.background = pygame.surface.Surface(SCREEN_SIZE).convert()
+    # paint black even the heavens
+    self.background.fill((0, 0, 0))
 
-# variables
-APP_NAME = "Orrery"
-SCREEN_SIZE = (640, 480)
+  def add_sat(self, entity):
+    self.entities[self.entity_id] = entity
+    entity.id = self.entity_id
 
-pygame.display.set_caption(APP_NAME)
+  def remove_sat(self, entity):
+    del self.entities[entity.id]
 
-screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
-screen_width, screen_height = SCREEN_SIZE
+  def get(self, entity_id):
+    if entity_id in self.entities:
+      return self.entities[entity_id]
+    else:
+      return None
 
-Fullscreen = False
+  def process(self, time_passed):
+    time_passed_seconds = time_passed / 1000.0
+    for entity in self.entities.values():
+      entity.process(time_passed_seconds)
 
-background = pygame.Surface(SCREEN_SIZE)
-background = background.convert()
-background.fill((0, 0 ,0))
+  def render(self, surface):
+    surface.blit(self.background, (0, 0))
+    for entity in self.entities.itervalues():
+      entity.render(surface)
 
-class satellite:
-  def __init__(self, name):
-    sat_name = name
-    sat_size = 0.
-    sat_speed = 0.
-    sat_location = Vector2(0, 0)
-    sat_destination = Vector2(0, 0)
-    sat_image = pygame.draw.ellipse(screen, (255, 255, 255), (carmara.width / 2, carmara.width / 2, 10, 10))
-    sat_id = 0.
+class GameEntity(object):
+  def __init__(self, world, name, image):
+    self.world = world
+    self.name = name
+    self.image = image
+    self.location = Vector2(0, 0)
+    self.destination = Vector2(0, 0)
+    self.speed = 0.
 
-while True:
-  for event in pygame.event.get():
-      if event.type == QUIT:
-        exit()
-      if event.type == KEYDOWN:
-        if event.key == K_f:
-          Fullscreen = not Fullscreen
-          if Fullscreen:
-            screen = pygame.display.set_mode(SCREEN_SIZE, FULLSCREEN, 32)
-          else:
-            screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
-        if event.key == K_q:
+class satellite(GameEntity):
+  def __init__(self, world, image):
+    GameEntity.__init__(self, world, "satellite", image)
+    self.id = 0.
+    self.name = name
+    self.location = Vector2(0, 0)
+    self.destination = Vector2(0, 0)
+    self.size = 0.
+    self.speed = 0.
+
+  def render(self, surface):
+    x, y = self.location
+    w, h = self.image.get_size()
+
+  def process(self, time_passed):
+    x, y = self.location
+
+    GameEntity.process(self, time_passed)
+
+def run():
+  pygame.init()
+
+  screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
+
+  world = World()
+
+  screen_width, screen_height = SCREEN_SIZE
+
+  clock = pygame.time.Clock()
+
+  # world.add_sat(satellite)
+
+  while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
           exit()
+        if event.type == KEYDOWN:
+          if event.key == K_f:
+            Fullscreen = not Fullscreen
+            if Fullscreen:
+              screen = pygame.display.set_mode(SCREEN_SIZE, FULLSCREEN, 32)
+            else:
+              screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
+          if event.key == K_q:
+            exit()
 
-  # paint black even the heavens
-  screen.blit(background, (0, 0))
+    time_passed = clock.tick(30)
 
-  # news of great import!
-  # circular heavenly bodies are defined by (plane, (colour - in rgb), (position), radius, width)
-  # elliptical bodies are defined by (plane, (colour - in rgb), (pos x, pos y, width, height))
+    world.process(time_passed)
+    world.render(screen)
 
-  # the center of the geocentric universe
-  carmara = pygame.draw.circle(screen, (255, 255, 255), ((screen_width / 2), (screen_height / 2)), screen_height / 2 - 100, 1)
+    # the center of the geocentric universe
+    carmara = pygame.draw.circle(screen, (255, 255, 255), ((screen_width / 2), (screen_height / 2)), screen_height / 2 - 100, 1)
 
-  sat1 = pygame.draw.ellipse(screen, (255, 255, 255), (carmara.width / 2, carmara.width / 2, 10, 10))
+    # set the wheels in motion
+    pygame.display.update()
 
-  pygame.display.update()
+    # show me that the arrow flies
+    print time_passed
+
+if __name__ == "__main__":
+  run()
